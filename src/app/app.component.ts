9a9eb24e23fa52e57ccb1fa716089d64f4c8cf3f
hossameldinfahmi <hossameldinfahmi@gmail.com>
2025-08-12 10:01:19 +0300

    feat: handle icons font

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { LoadingService } from './shared/loading.service';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,15 +9,26 @@ import { LoadingService } from './shared/loading.service';
   styleUrls: ['./app.component.scss'] 
 })
 export class AppComponent {
-   constructor(private loading: LoadingService) {
+  showNavbar = true;
+
+  constructor(private loading: LoadingService, private router: Router) {
+    this.handleNavbarVisibility();
     this.loadData();
-   }
+  }
 
   loadData() {
     this.loading.show();
-
     setTimeout(() => {
       this.loading.hide();
-    }, 2000); 
+    }, 2000);
+  }
+
+  handleNavbarVisibility() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        const currentUrl = event.urlAfterRedirects;
+        this.showNavbar = !(currentUrl.includes('/login') || currentUrl.includes('/forgot-password'));
+      });
   }
 }
